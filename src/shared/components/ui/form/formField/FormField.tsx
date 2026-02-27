4b2d50da001a004c1bba14de70acd18846654bc2
Oscar Lozano <olozano1194@gmail.com>
2026-02-27 12:52:57 -0500

    feat(ui): add reusable UI components
    - create FormField component

--- src/shared/components/ui/form/formField/FormField.tsx
+++ src/shared/components/ui/form/formField/FormField.tsx
@@ -1,12 +1,6 @@
 import { ReactNode } from "react";
 
-interface FormFieldProps {
-    label: ReactNode;
-    error?: string;
-    children: ReactNode;
-};
-
-const FormField = ({ label, error, children }: FormFieldProps) => {
+const FormField = ({ label, error, children }) => {
     return (
     <div className="w-full flex flex-col gap-1">
       <label className="w-full flex gap-3 font-semibold md:gap-3 md:justify-center md:p-3">
