fe82a47147cafada2c1b0ff23a59033bd79ec31a
Oscar Lozano <olozano1194@gmail.com>
2026-02-27 02:01:10 -0500

    feat(routing): update main route and create HomePage structure
    - change default route to HomePage
    - create HomePage container for landing components
    - prepare structure for hero, testimonials and othe section

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { HorseListPage } from '../features/horses/pages/HorseListPage';
+import LayoutAdminPage from '../features/horses/pages/LayoutAdminPage';
+import HomePage from '../features/horses/pages/HomePage';
+import LoginPage from '../features/horses/pages/auth/LoginPage';
 
 const router = createBrowserRouter(
 	[
 		{
 			path: '/',
-			element: <HorseListPage />
+			element: <LayoutAdminPage />,
+			children: [
+				{
+					index: true,
+					element: <HomePage />
+				}
+			],
 		},
+		{
+			path: '/login',
+			element: <LoginPage />,
+		}
 	],
 	{
 		basename: "/equine-verification-ui",
